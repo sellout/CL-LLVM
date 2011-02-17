459fd28f40b9b529568c7393116a7df235651892
Greg Pfeil <greg@technomadic.org>
2011-02-17 16:41:56 -0500

    A bunch of tutorial changes to make it work with SBCL and its package locks.

--- tutorial/chapter2.lisp
+++ tutorial/chapter2.lisp
@@ -15,15 +15,15 @@
   (defun read-token ()
     "Returns either a character or one of 'tok-eof, 'tok-def, 'tok-extern,
      'tok-identifier, or 'tok-number."
-    (flet ((read-char () (read-char *standard-input* nil nil)))
+    (flet ((get-char () (read-char *standard-input* nil nil)))
       (loop while (find last-char +whitespace+)
-        do (setf last-char (read-char)))
+        do (setf last-char (get-char)))
       (cond ((eql last-char nil) ; check for EOF, do not eat
              'tok-eof)
             ((alpha-char-p last-char)
              (setf *identifier-string*
                    (coerce (cons last-char
-                                 (loop do (setf last-char (read-char))
+                                 (loop do (setf last-char (get-char))
                                    while (alphanumericp last-char)
                                    collecting last-char))
                            'string))
@@ -35,19 +35,19 @@
                    (let ((*read-eval* nil))
                      (read-from-string
                       (coerce (cons last-char
-                                    (loop do (setf last-char (read-char))
+                                    (loop do (setf last-char (get-char))
                                       while (or (digit-char-p last-char)
                                                 (char= last-char #\.))
                                       collecting last-char))
                               'string))))
              'tok-number)
             ((eql last-char #\#) ; comment until end of line
-             (loop do (setf last-char (read-char))
+             (loop do (setf last-char (get-char))
                until (find last-char '(nil #\linefeed #\return)))
              (if (null last-char) 'tok-eof (read-token)))
             (t
              (let ((this-char last-char))
-               (setf last-char (read-char))
+               (setf last-char (get-char))
                this-char))))))
 
 ;;; abstract syntax tree
@@ -147,7 +147,7 @@
                       :message "unknown token when expecting an expression"))))
 
 (defun parse-bin-op-rhs (expression-precedence lhs)
-  (do () ()
+  (do () (nil)
     (let ((token-precedence (get-precedence *current-token*)))
       (if (< token-precedence expression-precedence)
         (return-from parse-bin-op-rhs lhs)
--- tutorial/chapter5.lisp
+++ tutorial/chapter5.lisp
@@ -1,5 +1,6 @@
 (defpackage kaleidoscope.chapter5
   (:use #:cl) ; would normally use #:llvm, but wanted to make usage clear
+  (:shadow #:condition)
   (:export #:toplevel))
 
 (in-package :kaleidoscope.chapter5)
@@ -15,15 +16,15 @@
   (defun read-token ()
     "Returns either a character or one of 'tok-eof, 'tok-def, 'tok-extern,
      'tok-identifier, or 'tok-number."
-    (flet ((read-char () (read-char *standard-input* nil nil)))
+    (flet ((get-char () (read-char *standard-input* nil nil)))
       (loop while (find last-char +whitespace+)
-        do (setf last-char (read-char)))
+        do (setf last-char (get-char)))
       (cond ((eql last-char nil) ; check for EOF, do not eat
              'tok-eof)
             ((alpha-char-p last-char)
              (setf *identifier-string*
                    (coerce (cons last-char
-                                 (loop do (setf last-char (read-char))
+                                 (loop do (setf last-char (get-char))
                                    while (alphanumericp last-char)
                                    collecting last-char))
                            'string))
@@ -40,19 +41,19 @@
                    (let ((*read-eval* nil))
                      (read-from-string
                       (coerce (cons last-char
-                                    (loop do (setf last-char (read-char))
+                                    (loop do (setf last-char (get-char))
                                       while (or (digit-char-p last-char)
                                                 (char= last-char #\.))
                                       collecting last-char))
                               'string))))
              'tok-number)
             ((eql last-char #\#) ; comment until end of line
-             (loop do (setf last-char (read-char))
+             (loop do (setf last-char (get-char))
                until (find last-char '(nil #\linefeed #\return)))
              (if (null last-char) 'tok-eof (read-token)))
             (t
              (let ((this-char last-char))
-               (setf last-char (read-char))
+               (setf last-char (get-char))
                this-char))))))
 
 ;;; abstract syntax tree
@@ -215,7 +216,7 @@
                       :message "unknown token when expecting an expression"))))
 
 (defun parse-bin-op-rhs (expression-precedence lhs)
-  (do () ()
+  (do () (nil)
     (let ((token-precedence (get-precedence *current-token*)))
       (if (< token-precedence expression-precedence)
         (return-from parse-bin-op-rhs lhs)
