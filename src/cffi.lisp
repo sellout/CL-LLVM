e59cf414662ca51ad4d332d47a080c21b6c8f847
terminal625 <zpersoncool@gmail.com>
2018-06-11 17:06:30 -0400

    add llvm 3.8

--- src/cffi.lisp
+++ src/cffi.lisp
@@ -88,10 +88,20 @@
                 (:default "libLLVM-3.1")
                 (:default "libLLVM-3.1svn")
                 (:default "libLLVM-3.0")))
-  (:unix (:or "libLLVM.so" "libLLVM.so.1" "libLLVM-3.6.so"
-              "libLLVM-3.1.so" "libLLVM-3.1.so.1" "libLLVM-3.1svn.so"
-              "libLLVM-3.1svn.so.1" "libLLVM-3.0.so" "libLLVM-3.0.so.1"))
+  (:unix (:or "libLLVM.so"
+	      "libLLVM.so.1"
+	      ;;added
+	      "libLLVM-3.8.so"
+	      "libLLVM-3.6.so"
+              "libLLVM-3.1.so"
+	      "libLLVM-3.1.so.1"
+	      "libLLVM-3.1svn.so"
+              "libLLVM-3.1svn.so.1"
+	      "libLLVM-3.0.so"
+	      "libLLVM-3.0.so.1"))
   (t (:or (:default "libLLVM")
+	  ;;added
+	  (:default "libLLVM-3.8")
           (:default "libLLVM-3.6")
           (:default "libLLVM-3.1")
           (:default "libLLVM-3.1svn")
