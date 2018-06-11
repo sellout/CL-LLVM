f7ff40e3df0d30a7de69407655364bec06ef5d87
terminal625 <zpersoncool@gmail.com>
2018-06-11 17:22:30 -0400

    wtf?

--- tutorial/cffi.lisp
+++ tutorial/cffi.lisp
@@ -1,8 +1,11 @@
+;;wtf?
+
 ;;; This just makes sure the library gets loaded for use by LLVM
+#+nil
+(progn
+  (cffi:define-foreign-library libkaleidoscope
+    (t (:default #.(namestring
+		    (asdf:system-relative-pathname :kaleidoscope256
+						   "tutorial/.libs/libkaleidoscope")))))
 
-(cffi:define-foreign-library libkaleidoscope
-  (t (:default #.(namestring
-                  (asdf:system-relative-pathname :kaleidoscope
-                                                 "tutorial/.libs/libkaleidoscope")))))
-
-(cffi:use-foreign-library libkaleidoscope)
+  (cffi:use-foreign-library libkaleidoscope))
--- tutorial/test.sh
+++ tutorial/test.sh
@@ -4,42 +4,42 @@
 ## and the actual output.
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter2:toplevel)' \
          <tutorial/chapter2.k \
          2>&1 \
          | diff -ub tutorial/chapter2.out -
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter3:toplevel)' \
          <tutorial/chapter3.k \
          2>&1 \
          | diff -ub tutorial/chapter3.out -
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter4:toplevel)' \
          <tutorial/chapter4.k \
          2>&1 \
          | diff -ub tutorial/chapter4.out -
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter5:toplevel)' \
          <tutorial/chapter5.k \
          2>&1 \
          | diff -ub tutorial/chapter5.out -
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter6:toplevel)' \
          <tutorial/chapter6.k \
          2>&1 \
          | diff -ub tutorial/chapter6.out -
 
 dx86cl64 --quiet --batch \
-         --eval '(asdf:load-system :kaleidoscope :verbose nil)' \
+         --eval '(asdf:load-system :kaleidoscope256 :verbose nil)' \
          --eval '(kaleidoscope.chapter7:toplevel)' \
          <tutorial/chapter7.k \
          2>&1 \
